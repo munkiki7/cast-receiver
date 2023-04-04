82264ffee1e172657ee382f23ee2af81d54f3135
Andriy Marshalek <amarshalek@ninevastudios.com>
2023-04-04 11:47:08 +0300

    syntax

--- js/receiver.js
+++ js/receiver.js
@@ -268,7 +268,7 @@ castReceiverOptions.customNamespaces[CHANNEL] = cast.framework.system.MessageTyp
  */
 // castReceiverOptions.queue = new CastQueue();
 
-context.addCustomMessageListener(CHANNEL,  function(customEvent) {
+context.addCustomMessageListener(CHANNEL, customEvent => {
   // castDebugLogger.info(LOG_RECEIVER_TAG, 'Message received.');
   // castDebugLogger.info(LOG_RECEIVER_TAG, customEvent.data);
 });
